a37b4701a38659649c20c7eeb90cac080c361422
Reshma S Raveendran <reshmasraveendran@gmail.com>
2020-04-29 10:42:49 +0530

    Login

--- src/pages/Login/Component.tsx
+++ src/pages/Login/Component.tsx
@@ -9,7 +9,7 @@ import { SignInError } from '../../core/model/error.model';
 export const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
 
-const SigninSignupDialog = () => {
+const LoginComponent = () => {
     const dispatch = useDispatch();
     const { isSignedIn } = useSelector((state: DefaultStore) => state.user);
 
@@ -76,4 +76,4 @@ const SigninSignupDialog = () => {
     );
 };
 
-export default SigninSignupDialog;
+export default LoginComponent;
